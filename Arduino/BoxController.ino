6fda2eb994c474a59295d5d57194ca041f2a20bc
Henrik Ginnerup <hginnerup@hotmail.com>
2021-03-10 14:55:56 +0100

    Made fan controller use interupts

--- Arduino/BoxController.ino
+++ Arduino/BoxController.ino
@@ -1,20 +1,34 @@
 #include "headers/BoxController.h"
 
 
-#define FAN_READ_PIN 5
-#define FAN_WRITE_PIN 6
-#define BUTTON_TOGGLE_FANS 2
-#define BUTTON_TOGGLE_LIGHT 3
-#define LED_STRIP_PIN 11
+#define FAN_READ_PIN 3
+#define FAN_WRITE_PIN 4
+#define BUTTON_TOGGLE_FANS 5
+#define BUTTON_TOGGLE_LIGHT 6
+#define LED_STRIP_PIN 7
 #define LED_STRIP_LENGTH 50
 
 auto fanController = FanController<FAN_READ_PIN, FAN_WRITE_PIN>(0);
 auto ledController = LedController<LED_BUILTIN>();
 
 auto ledStrip = LedStripController<LED_STRIP_PIN, LED_STRIP_LENGTH>();
+void ledStripOnRpmUpdate(uint16_t rpm);
+auto fanController = FanController<FAN_READ_PIN, FAN_WRITE_PIN, ledStripOnRpmUpdate>(0);
 auto fanButton = ButtonController(BUTTON_TOGGLE_FANS, nullptr, setFanState);
 auto lightButton = ButtonController(BUTTON_TOGGLE_LIGHT, switchLightStripIntensity, nullptr);
 
+
+uint8_t lightStripIntencity = 64;
+void ledStripOnRpmUpdate(uint16_t rpm) {
+  uint8_t hue = 255*2000/rpm-200;
+  CHSV hsv = CHSV(hue, 255, lightStripIntencity);
+  for(int i=0; i<LED_STRIP_LENGTH; i++)
+    ledStrip.setColor(i, CRGB(hsv));
+  ledStrip.Show();
+
+  Serial.print("Rpm: ");
+  Serial.println(rpm);
+}
 void setFanState(bool state) {
   if(state) {
     ledController.turnOn();
--- Arduino/headers/FanController.h
+++ Arduino/headers/FanController.h
@@ -2,10 +2,30 @@
 
 #include "BoxController.h"
 
-template <pin speedReadPin, pin speedWritePin>
+template <pin speedReadPin, pin speedWritePin, void (*onRpmUpdated)(uint16_t rpm)>
 class FanController
 {
-    uint8_t targetPower;
+    static uint8_t targetPower;
+    //static uint32_t microsAtFalling;
+    static uint32_t millisAtRpmUpdate;
+    static uint16_t rpm;
+
+    static void interuptFanSenseRising() {
+        static uint32_t lastTime = micros();
+        uint32_t time = micros();
+        uint32_t timeDiff = time - lastTime;
+        lastTime = time;
+        
+        rpm = getRpm(timeDiff);
+
+
+        // uint32_t microsDiff = micros() - microsAtFalling;
+        millisAtRpmUpdate = millis();
+
+        // rpm = getRpm(microsDiff);
+        if(onRpmUpdated != nullptr)
+            onRpmUpdated(rpm);
+    }
 
 public:
     FanController(uint8_t targetPower)
@@ -13,6 +33,13 @@ public:
         pinMode(speedReadPin, INPUT_PULLUP);
         pinMode(speedWritePin, OUTPUT);
         this->setPower(targetPower);
+
+        attachInterrupt(
+            digitalPinToInterrupt(speedReadPin),
+            interuptFanSenseRising,
+            FALLING
+        );
+
     }
 
     void setPower(uint8_t power)
@@ -21,20 +48,37 @@ public:
         analogWrite(speedWritePin, power);
     }
 
-    uint32_t getPulseTimeMicros()
+    static uint32_t getPulseTimeMicros()
     {
         return pulseIn(speedReadPin, HIGH, 1E6); // Timeout at 10 rpm -> quaterRevolutionMicroseconds / 10rpm = 1E6
     }
-    uint16_t getRpm(uint32_t halfRevolutionMicros) {
-        
-        return 15E6 / halfRevolutionMicros; // Microseconds per minute / (quaterRevolutionMicroseconds * 4)
+
+    static uint16_t getRpm(uint32_t halfRevolutionMicros)
+    {
+        //return 15E6 / halfRevolutionMicros; // Microseconds per minute / (quaterRevolutionMicroseconds * 4)
+        return 30E6 / halfRevolutionMicros; // Microseconds per minute / (quaterRevolutionMicroseconds * 4)
     }
-    uint16_t getRpm() {
-        return getRpm(getPulseTimeMicros());
+
+    static uint16_t getRpm()
+    {
+        //return getRpm(getPulseTimeMicros());
+        if(millis() - millisAtRpmUpdate > 15000) // if less than 1 rpm
+            return 0;
+
+        return rpm;
     }
     
-    uint8_t getPowerTarget()
+    static uint8_t getPowerTarget()
     {
-        return this->targetPower;
+        return targetPower;
     }
 };
+
+template <pin speedReadPin, pin speedWritePin, void (*onRpmUpdated)(uint16_t rpm)>
+uint8_t FanController<speedReadPin, speedWritePin, onRpmUpdated>::targetPower = 0;
+
+template <pin speedReadPin, pin speedWritePin, void (*onRpmUpdated)(uint16_t rpm)>
+uint32_t FanController<speedReadPin, speedWritePin, onRpmUpdated>::millisAtRpmUpdate = 0;
+
+template <pin speedReadPin, pin speedWritePin, void (*onRpmUpdated)(uint16_t rpm)>
+uint16_t FanController<speedReadPin, speedWritePin, onRpmUpdated>::rpm = 0;
